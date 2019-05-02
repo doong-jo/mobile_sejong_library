545374a42578e02a800689bb84151e90d1ffc829
HyoungHoKim <dkdemfp2@naver.com>
2019-05-02 22:01:06 +0900

    Modify : Change activity name and xml, and use @strings

--- app/src/main/java/com/fourB/library/MainActivity.java
+++ app/src/main/java/com/fourB/library/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity
         mEbookView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), WebViewActivity.class);
+                Intent intent = new Intent(getApplicationContext(), EbookActivity.class);
                 startActivity(intent);
             }
          });
