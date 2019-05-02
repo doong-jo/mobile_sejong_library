63b7f34690878eb851f7e7e227285eda8d1e84fa
HyoungHoKim <dkdemfp2@naver.com>
2019-05-02 20:54:29 +0900

    Add : WebView_Ebook

---
+++ app/src/main/java/com/fourB/library/WebViewActivity.java
@@ -0,0 +1,54 @@
+package com.fourB.library;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    private  WebView mWebView;//Mobile Web View
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+
+        setTitle("E-Book");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mWebView=(WebView)findViewById(R.id.activity_main_webview);
+
+        WebSettings mWebSettings=mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
+        mWebSettings.setLoadWithOverviewMode(true);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.loadUrl("https://ebook.sejong.ac.kr/m/main.asp");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        finish();
+    }
+
+}
